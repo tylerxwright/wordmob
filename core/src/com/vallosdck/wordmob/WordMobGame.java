29f6e26539f15ab21aaee18ae372b26674dde8cf
Tyler Wright <tylerxwright@gmail.com>
2024-09-25 14:28:27 -0400

    test-1 - break

--- core/src/com/vallosdck/wordmob/WordMobGame.java
+++ core/src/com/vallosdck/wordmob/WordMobGame.java
@@ -18,13 +18,17 @@ public class WordMobGame extends DirectedGame {
 
 	@Override
 	public void create () {
+		int zero = 0;
+		String str = '';
+		double divideByZero = (1/zero)+str;
+
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
 		Assets.instance.init(new AssetManager());
-		GameManager.instance.init();
+			GameManager.instance.init();
 		GameManager.instance.reset();
 
-		setScreen(new GameScreen(this));
+		setScreen(new GameScreen(this));;
 		//setScreen(new MenuScreen(this));
 	}
 
